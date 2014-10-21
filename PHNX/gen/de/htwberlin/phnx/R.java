ce0def9eca44999240efa26ae2080d7a0b86d86e
DustinF <bosche@hotmail.de>
2014-10-21 17:03:54 +0200

    added main menu xml + activity

--- PHNX/gen/de/htwberlin/phnx/R.java
+++ PHNX/gen/de/htwberlin/phnx/R.java
@@ -25,12 +25,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080008;
         public static final int button1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int radio0=0x7f080004;
+        public static final int radio1=0x7f080005;
+        public static final int radio2=0x7f080006;
+        public static final int radioGroup1=0x7f080003;
+        public static final int searchID=0x7f080000;
+        public static final int searchView1=0x7f080002;
+        public static final int view1=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_main_menu=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -38,7 +45,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int equip=0x7f050006;
         public static final int hello_world=0x7f050001;
+        public static final int organisation=0x7f050005;
+        public static final int person=0x7f050004;
+        public static final int placeholder=0x7f050003;
     }
     public static final class style {
         /** 
