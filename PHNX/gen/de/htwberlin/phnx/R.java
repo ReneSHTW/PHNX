40f59bd3bc9337cfc6d6466ac9a34e933c4dc997
DustinF <bosche@hotmail.de>
2014-10-24 15:51:33 +0200

    leviosa

--- PHNX/gen/de/htwberlin/phnx/R.java
+++ PHNX/gen/de/htwberlin/phnx/R.java
@@ -25,19 +25,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080001;
-        public static final int radio0=0x7f080004;
-        public static final int radio1=0x7f080005;
-        public static final int radio2=0x7f080006;
-        public static final int radioGroup1=0x7f080003;
-        public static final int searchID=0x7f080000;
-        public static final int searchView1=0x7f080002;
-        public static final int view1=0x7f080007;
+        public static final int action_settings=0x7f080011;
+        public static final int button1=0x7f080009;
+        public static final int checkBox1=0x7f080007;
+        public static final int editText1=0x7f080001;
+        public static final int editText2=0x7f080002;
+        public static final int editText3=0x7f080005;
+        public static final int imageView1=0x7f080010;
+        public static final int radio0=0x7f08000c;
+        public static final int radio1=0x7f08000d;
+        public static final int radio2=0x7f08000e;
+        public static final int radioGroup1=0x7f08000b;
+        public static final int searchID=0x7f080008;
+        public static final int searchView1=0x7f08000a;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080004;
+        public static final int textView4=0x7f080006;
+        public static final int view1=0x7f08000f;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_main_menu=0x7f030001;
+        public static final int activity_create_equipment=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_main_menu=0x7f030002;
+        public static final int first_use_welcome_screen=0x7f030003;
+        public static final int profile_create_1=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -45,11 +57,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int equip=0x7f050006;
+        public static final int createCard=0x7f050006;
+        public static final int equip=0x7f05000b;
         public static final int hello_world=0x7f050001;
-        public static final int organisation=0x7f050005;
-        public static final int person=0x7f050004;
-        public static final int placeholder=0x7f050003;
+        public static final int myCard=0x7f050007;
+        public static final int organisation=0x7f05000a;
+        public static final int person=0x7f050009;
+        public static final int placeholder=0x7f050008;
+        public static final int selectType=0x7f050003;
+        public static final int welcomeMessage=0x7f050005;
+        public static final int welcomePhnx=0x7f050004;
     }
     public static final class style {
         /** 
--- PHNX/src/de/htwberlin/phnx/MainActivity.java
+++ PHNX/src/de/htwberlin/phnx/MainActivity.java
@@ -4,25 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.first_use_welcome_screen);
 	}
-//kommentar
-	//zweiter Kommentar
-	//AJAJAJAJAJAJJAJAAJJA
-	//LEVIOSA
-	//LEVIOZAAAAAA Supergeil
-	//sdljfklsdjfkldsjflkdsjfl
+
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	setContentView(R.layout.first_use_welcome_screen);
+            }
+        });
 		return true;
 	}
 
@@ -37,4 +42,6 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
 }
