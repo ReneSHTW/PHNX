0b5f9b72e976893726775c932594e9cfa3f5890f
DustinF <bosche@hotmail.de>
2014-10-27 15:15:01 +0100

    xml update

--- PHNX/gen/de/htwberlin/phnx/R.java
+++ PHNX/gen/de/htwberlin/phnx/R.java
@@ -25,12 +25,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
+        public static final int action_settings=0x7f08001a;
         public static final int button1=0x7f080009;
         public static final int checkBox1=0x7f080007;
         public static final int editText1=0x7f080001;
+        public static final int editText10=0x7f080017;
+        public static final int editText11=0x7f080018;
+        public static final int editText12=0x7f080019;
         public static final int editText2=0x7f080002;
         public static final int editText3=0x7f080005;
+        public static final int editText4=0x7f080011;
+        public static final int editText5=0x7f080012;
+        public static final int editText6=0x7f080013;
+        public static final int editText7=0x7f080014;
+        public static final int editText8=0x7f080015;
+        public static final int editText9=0x7f080016;
         public static final int imageView1=0x7f080010;
         public static final int radio0=0x7f08000c;
         public static final int radio1=0x7f08000d;
