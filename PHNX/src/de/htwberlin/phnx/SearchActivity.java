96d70f0ed453bd1069ef2d31cb980bc60837345b
Dustin Feurich <s0540042@htw-berin.de>
2014-11-07 10:11:53 +0100

    updated organization card xml and search activity + xml

--- PHNX/src/de/htwberlin/phnx/SearchActivity.java
+++ PHNX/src/de/htwberlin/phnx/SearchActivity.java
@@ -1,13 +1,37 @@
 package de.htwberlin.phnx;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SearchActivity extends Activity {
+public class SearchActivity extends Activity implements OnClickListener {
+
+	private Button findBtn;
+	private Intent intent;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search);
+		findBtn = (Button) findViewById(R.id.button1);
+		findBtn.setOnClickListener(this);
 	}
-	
+
+	@Override
+	public void onClick(View v) {
+		{
+			switch (v.getId()) {
+			case R.id.button1:
+				intent = new Intent(SearchActivity.this,
+						SearchResultActivity.class);
+				startActivity(intent);
+				break;
+
+			}
+
+		}
+	}
+
 }
---
+++ PHNX/src/de/htwberlin/phnx/SearchResultActivity.java
@@ -0,0 +1,16 @@
+package de.htwberlin.phnx;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchResultActivity extends Activity {
+	
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search_result);
+	}
+	
+	
+
+}
