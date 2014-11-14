794d1c257d241c66e2501cc5b69a5d600e1f3734
Rene Schumann <s0539752@htw-berlin.de>
2014-11-14 09:00:58 +0100

    Update PHNX.java

--- PHNX/src/de/htw/berlin/phoenix/PHNX.java
+++ PHNX/src/de/htw/berlin/phoenix/PHNX.java
@@ -9,4 +9,10 @@ public interface PHNX {
 	Iterator<PHNXSearchResult> getSearchResults(boolean buttonPerson,
 			boolean buttonOrganization, boolean buttonResource,
 			boolean buttonMap, String searchInput);
+	
+	PHNXResource getPHNXResource();
+	
+	PHNXOrganization getOrganziation();
+	
+//	PHNXMap getPHNXMap();
 }
