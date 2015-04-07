ec42eb043bed63dd1b4984a97ffc4b9949d864fa
019a <olga.sylenko@gmail.com>
2015-04-07 22:19:35 +0200

    Updated interface documentation

--- PHNX/src/de/htw/berlin/PHNX/interfaces/PHNXBusinessCard.java
+++ PHNX/src/de/htw/berlin/PHNX/interfaces/PHNXBusinessCard.java
@@ -1,17 +1,24 @@
 package de.htw.berlin.PHNX.interfaces;
 
 /** Public interface PHNXBusinessCard represents methods to provide access to the information about persons saved in  
- * in the Phoenix app. Each person, including the user/owner of the app, has a PHNXBusinessCard.
- * The user of the app can create their personal business card as well as business cards for other persons to be able to search easily for them and for the resources associated with these people, even if they don't use the PHNX app.
- * Hence a business card created for another person (not the owner of the app) has to be replaceable with the business card created by the actual person represented by the business card.
- * The person, whom the PHNXBusinessCard represents, must have entered the following necessary pieces of information for easy identification and search:
- * - Their name (can also include their nickname);
+ * in the Phoenix app. Every person entered in the app has a PHNXBusinessCard: the user/owner of the app, as well as each person 
+ * whose data is entered in the app. 
+ * The user of the app can create their personal business card as well as business cards for other persons to be able to search easily
+ * for them and for the resources associated with these people, even if they don't use the PHNX app.
+ * The implementation has to make sure that a business card created for another person (not the owner of the app) has to be replaceable 
+ * with the business card created by the actual person represented by the business card.
+ * The person represented by the PHNXBusinessCard must enter the following necessary pieces of information for easy identification and search:
+ * - Their name (first name, last name, several middle names, can also include their nickname);
  * - Their contact information which has to at least contain an easily identifiable, unique resource, like an email address or a URI;
- * - Their organization - since one of the major requirement from our customers was that each person using this app has to be sent to the area by an organization. Adventure seekers and "lone wolves" cannot be taken into account in crisis areas.
- * - Their profession - the customers found it useful to have the information about the person's actual education/degree/sphere of training, and not only the role they currently play in their organization.
+ * - Their organization. It was one of the major customer requirements that each person using this app has to belong to an organization. 
+ * Adventure seekers and "lone wolves" should not be taken into account in catastrophe relief operations.
+ * - Their profession - the customers found it useful to have the information about the person's actual education/degree/sphere of training, 
+ * and not only the role they currently play in their organization. It might be relevant especially in medical matters (e.g. whether a person
+ * is qualified to perform a surgery). 
  * - Their skills - which could be any relevant skills (medical, language etc) that the person can offer to be used during the catastrophe relief operation.
- * - Their arrival and departure dates - since a person deployed in a catastrophe relief operation normally knows their arrival and departure dates. Those dates may help determine the relevance of information generated and shared by those people.
- * - Their picture - optional.
+ * - Their arrival and departure dates - since a person deployed in a catastrophe relief operation normally knows their arrival and departure dates. 
+ * Those dates may help determine the relevance of information generated and shared by those people.
+ * - Optional: Their picture.
  *
  */
 
@@ -33,13 +40,13 @@ public interface PHNXBusinessCard {
 	public PHNXName getName();
 	
 	/**getContact() provides access to a stored person's contact information.
-	 * The contact will consist of email address, optional phone number, optional post address, optional link to a personal website.
+	 * The contact will consist of obligatory email address, optional phone number, optional post address, optional link to a personal website.
 	 * 
 	 * @return the contact information of the person according to the standard defined in PHNX interface
 	 */
 	public PHNXContact getContact();
 
-	/**getOrganization() provides access to the organization information of the person.
+	/**getOrganization() provides access to the organization information of the person (which organization the person is working for during the catastrophe relief).
 	 * The organization will consist of organization name, optional logo picture, organization Internet address, organization email address, and contact person.
 	 * 
 	 * @return the organization information of the person according to the standard defined in PHNXContact interface
@@ -56,7 +63,8 @@ public interface PHNXBusinessCard {
 	public PHNXResource getProfession() throws SharkKBException;
 
 	/**getPrintableProfessionalDegree() provides access to the information on the person's professional degree and/or qualifications.
-	 * The professional degree here means the learned skills which qualify the person to do a certain job, since the person's education may not reflect in their current job position (e.g. medical nurse, a rocket scientist, a brain surgeon).
+	 * The professional degree here means the learned skills which qualify the person to do a certain job, since the person's education may not be reflected 
+	 * in their current job position (e.g. medical nurse, a rocket scientist, a brain surgeon).
 	 * 
 	 * @return String containing the person's professional degree/qualifications in printable form
 	 */
