f6e05dc61e57e5d403e750b907dfba22d07ef17b
019a <olga.sylenko@gmail.com>
2015-04-08 14:36:37 +0200

    Added comments in PHNXPicture interface

--- PHNX/src/de/htw/berlin/PHNX/interfaces/PHNXPicture.java
+++ PHNX/src/de/htw/berlin/PHNX/interfaces/PHNXPicture.java
@@ -1,8 +1,21 @@
+/**The interface PHNXPicture contains methods allowing access to pictures in PHNX app.
+ * Since a picture is not uniquely defined for all possible different implementations,
+ * this interface is used as a wrapper for possible picture formats and implementations.
+ * 
+ */
 package de.htw.berlin.PHNX.interfaces;
 
 /*es gibt ein Topic welches Picture heiﬂt*/
 public interface PHNXPicture {
+	/**Allows access to the picture/photo
+	 * 
+	 * @return path to the picture/photo
+	 */
 	String getPicture();
 	
+	/**Sets the path to where the picture/phone is found for this implementation
+	 * 
+	 * @param picturePath the path to the picture
+	 */
 	 void setPicture(String picturePath);
 }
